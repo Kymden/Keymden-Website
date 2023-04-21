fc1332b50e5a650f6e6348730e5e6e5aca506f49
Enock <enock.mokua@student.moringaschool.com>
2023-04-21 22:01:45 +0300

    style the header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,24 @@
+import { Link } from "react-router-dom";
 import "./header.css";
 
 export default function Header() {
-  return <header>This is the header</header>;
+  return (
+    <header>
+      <div>
+        <Link to="/">Logo</Link>
+
+        <span>
+          <Link to="/services">Services</Link>
+          <Link to="/projects">Projects</Link>
+          <Link to="/about">About</Link>
+          <Link to="/about">About</Link>
+          <Link to="/about">About</Link>
+        </span>
+
+        <Link to="/contact" className="contact-button">
+          Contact Us
+        </Link>
+      </div>
+    </header>
+  );
 }
