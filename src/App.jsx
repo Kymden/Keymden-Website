43eca29c31b495ef551c5c0bb43a5ebb2422db52
Enock <enock.mokua@student.moringaschool.com>
2023-04-21 19:54:05 +0300

    set up the footer and header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/landing/landing";
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Landing />}></Route>
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
