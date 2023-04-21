7b33dac487a7dc37fa02a7589614d24b4e63f46e
Enock <enock.mokua@student.moringaschool.com>
2023-04-21 20:09:53 +0300

    connect services page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import About from "./pages/About/about";
 import NotFound from "./pages/notFound/notfound";
+import Services from "./pages/services/services";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />}></Route>
         <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
