c5d98a92599042d0da1a3027c775f6458cac9b02
Enock <enock.mokua@student.moringaschool.com>
2023-04-22 06:30:04 +0300

    creat estructure of landing 1

--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -1,5 +1,27 @@
+import { Link } from "react-router-dom";
 import "./landing.css";
 
 export default function Landing() {
-  return <main id="landing">This is the landing page</main>;
+  return (
+    <main id="landing">
+      <section className="landing-1">
+        <p className="landing-par1">WELCOME TO KEYMDEN SOLUTIONS</p>
+        <h1>
+          Simplify Your Digital <br />
+          Adoption
+        </h1>
+
+        <p className="landing-par">
+          Experience streamlined operations and accelerated growth with our
+          customized and scalable technology solutions, designed to meet the
+          unique needs of businesses of any size and industry.
+        </p>
+
+        <div>
+          <Link to="">Request A Quote</Link>
+          <Link to="">How We Work</Link>
+        </div>
+      </section>
+    </main>
+  );
 }
