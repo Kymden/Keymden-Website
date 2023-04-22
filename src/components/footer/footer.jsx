88147f24cad3b6e65d3248dce4e08adf51ad51c9
Enock <enock.mokua@student.moringaschool.com>
2023-04-23 02:42:47 +0300

    create structure for foot

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,5 +1,26 @@
 import "./footer.css";
 
+import { FaInstagram, FaTwitter, FaLinkedin, FaFacebook } from "react-icons/fa";
+
 export default function Footer() {
-  return <footer>This is the footer</footer>;
+  return (
+    <footer>
+      <div id="foot-1"></div>
+
+      <div id="foot-2">
+        <span id="foot-2-head">&copy; 2023 Keymden Solutions</span>
+        <span id="foot-2-body">
+          <p>Terms of use</p>
+          <p>Privacy policy</p>
+          <p>Licence</p>
+        </span>
+        <span id="foot-2-foot">
+          <FaFacebook />
+          <FaTwitter />
+          <FaLinkedin />
+          <FaInstagram />
+        </span>
+      </div>
+    </footer>
+  );
 }
