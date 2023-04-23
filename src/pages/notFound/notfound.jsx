a7cbc8af3975d6ac6274d2b42deb9ddff9033e96
Enock <enock.mokua@student.moringaschool.com>
2023-04-23 06:12:26 +0300

    add image for page not found

--- src/pages/notFound/notfound.jsx
+++ src/pages/notFound/notfound.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import "./notfound.css";
-
+import svg from "../../assets/3828537.jpg";
 export default function NotFound() {
   return (
     <main id="notfound">
       <div>
-        <h2>We've lost this page.</h2>
+        <img src={svg} alt="notfound" />
+        <h2>Page not found.</h2>
         <p>
           Sorry the page you are looking for doesnt exist or has been moved.
         </p>
