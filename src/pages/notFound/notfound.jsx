9fec5c195280661c945db19cca8ca0ec726d4cef
Enock <enock.mokua@student.moringaschool.com>
2023-04-23 06:01:19 +0300

    style the not found page

--- src/pages/notFound/notfound.jsx
+++ src/pages/notFound/notfound.jsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
 import "./notfound.css";
 
 export default function NotFound() {
-  return <main id="notfound">This does not exist</main>;
+  return (
+    <main id="notfound">
+      <div>
+        <h2>We've lost this page.</h2>
+        <p>
+          Sorry the page you are looking for doesnt exist or has been moved.
+        </p>
+        <Link to="/">Back to Home</Link>
+      </div>
+    </main>
+  );
 }
