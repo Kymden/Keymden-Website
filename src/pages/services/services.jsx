bf893a13d37801502296c0bdbdb40649a5fc83ce
Enock <enock.mokua@student.moringaschool.com>
2023-04-24 20:05:41 +0300

    create structuure for the services page

--- src/pages/services/services.jsx
+++ src/pages/services/services.jsx
@@ -1,5 +1,53 @@
 import "./services.css";
 
 export default function Services() {
-  return <main id="services">This is the services pages</main>;
+  return (
+    <main id="services">
+      <section id="services-1">
+        <h2>
+          We <strong>are</strong> versatile
+        </h2>
+        <p>
+          Enabling a variety of industries through the provision of tailored
+          solutions and services.
+        </p>
+      </section>
+
+      <section className="services-2">
+        <h3>Coding and programming</h3>
+        <p>
+          We believe that code and data is a means to the end. Learn proper
+          probelm solving with us.
+        </p>
+      </section>
+
+      <section className="services-3">
+        <h3>Software development</h3>
+        <p>
+          We power existing businesses through inhouse solutions that are secure
+          and fast. Focus on growth we will manage it for you
+        </p>
+      </section>
+
+      <section className="services-2">
+        <h3>Technical support</h3>
+        <p>
+          We also offer hardware solutions for your businesses exprience
+          discounted maintenance and superior hardware solutions.
+        </p>
+      </section>
+
+      <section className="services-3">
+        <h3>Insunity Fund</h3>
+        <p>
+          Some of our students have gone on to create solutions that are shaping
+          and changing the future of Africa. These solutions exist within SMEs.
+          Learn how you can partner with them.
+        </p>
+      </section>
+      {/* <section className="services-2"></section>
+      <section className="services-3"></section>
+      <section className="services-2"></section> */}
+    </main>
+  );
 }
